fa69c60139617f314f4f2afd1bc8ff00350612ea
Hammer-Inc <Avrami.hammer@gmail.com>
2020-06-09 23:49:36 +1000

    Adjust sizing margins to reduce wasted space

--- src/components/home/IconSection.js
+++ src/components/home/IconSection.js
@@ -5,18 +5,34 @@ import IconCard from "./IconCard";
 import Icon from "@material-ui/core/Icon";
 import ErrorBoundary from "../ErrorBoundary";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
+import withStyles from "@material-ui/core/styles/withStyles";
 
 const iconSize = 30;
 
+const styles = (theme) => ({
+  header: {
+    marginBottom: `${theme.spacing(8)}px`,
+    [theme.breakpoints.down("md")]: {
+      marginBottom: `${theme.spacing(7)}px`,
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: `${theme.spacing(6)}px`,
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginBottom: `${theme.spacing(5)}px`,
+    }
+  }
+});
+
 function IconSection(props) {
-  const { header, values, isDemo } = props;
+  const {header, values, isDemo, classes} = props;
   const isLgUp = useMediaQuery(theme => theme.breakpoints.up("up"));
   const isMdUp = useMediaQuery(theme => theme.breakpoints.up("md"));
   const isSmUp = useMediaQuery(theme => theme.breakpoints.up("sm"));
   return (
     <div>
       <div className="container-fluid lg-p-top">
-        <Typography variant="h3" align="center" className="lg-mg-bottom">
+        <Typography variant="h3" align="center" className={classes.header}>
           {header}
         </Typography>
         <div className="container-fluid">
@@ -24,23 +40,23 @@ function IconSection(props) {
             {values.map((element, i) => {
               let ItemIcon = <ErrorBoundary variant={"circle"}><Icon style={{fontSize: iconSize}}>{element.icon}</Icon></ErrorBoundary>;
               return <Grid
-                  item
-                  xs={6}
-                  md={4}
-                  data-aos={isDemo ? undefined : "zoom-in-up"}
-                  data-aos-once={true}
-                  data-aos-duration={1000}
-                  data-aos-delay={
-                    isMdUp ? Math.min(Math.floor(i / 3) * 200, 300) : Math.min(Math.floor(i / 2) * 200, 600)
-                  }
-                  key={element.headline}
+                item
+                xs={6}
+                md={4}
+                data-aos={isDemo ? undefined : "zoom-in-up"}
+                data-aos-once={true}
+                data-aos-duration={1000}
+                data-aos-delay={
+                  isMdUp ? Math.min(Math.floor(i / 3) * 200, 300) : Math.min(Math.floor(i / 2) * 200, 600)
+                }
+                key={element.headline}
               >
                 <ErrorBoundary variant={"rect"} width={200} height={200}>
                   <IconCard
-                      Icon={ItemIcon}
-                      color={element.color}
-                      headline={element.headline}
-                      text={element.text}
+                    Icon={ItemIcon}
+                    color={element.color}
+                    headline={element.headline}
+                    text={element.text}
                   />
                 </ErrorBoundary>
               </Grid>;
@@ -56,7 +72,8 @@ function IconSection(props) {
 IconSection.propTypes = {
   header: PropTypes.string.isRequired,
   values: PropTypes.arrayOf(PropTypes.object).isRequired,
-  isDemo: PropTypes.bool
+  isDemo: PropTypes.bool,
+  classes: PropTypes.object.isRequired
 };
 
-export default IconSection;
+export default withStyles(styles)(IconSection);
